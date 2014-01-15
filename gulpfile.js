74c865e74a8a6afb1b57eed82e72d4c11577ab14
Joel Kemp <mrjoelkemp@gmail.com>
2014-01-15 16:12:56 -0500

    Added watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@ var gulp    = require('gulp'),
     uglify  = require('gulp-uglify'),
     concat  = require('gulp-concat');
 
+var scripts = ['vendor/diff_patch/*.js', 'src/*.js'];
+
 gulp.task('scripts', function() {
+
+  gulp.src(scripts)
+    .pipe(concat('jquery.recorder.unmin.js'))
+    .pipe(gulp.dest('dist'));
+
   // Google's diff patch should be prepended
-  gulp.src(['vendor/**/*.js', 'src/*.js'])
+  gulp.src(scripts)
     .pipe(concat('jquery.recorder.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
@@ -14,7 +21,7 @@ gulp.task('default', function() {
   gulp.run('scripts');
 
   // Watch files and run tasks if they change
-  gulp.watch('src/js/**', function() {
+  gulp.watch(scripts, function () {
     gulp.run('scripts');
   });
 });
